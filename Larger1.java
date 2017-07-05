324b7804101c4c80a00e62f84ea068b348198ee8
KaaviyaMani <mkaaviya6897@gmail.com>
2017-07-05 12:25:48 +0530

    Create Larger1.java

---
+++ Larger1.java
@@ -0,0 +1,32 @@
+# aaa
+import java.io.*;
+import java.util.*;
+public class Larger1{
+  
+  public static void main(String args[])throws IOException{
+     Scanner sc=new Scanner (System.in);  
+     System.out.println("Enter the numbers");
+                      int a=sc.nextInt();
+                      int b=sc.nextInt();
+                      int c=sc.nextInt();
+                      int max=0;
+                      if(a>b)
+                      {
+                        
+                        max=a;
+                        }
+                        else if (b>c){
+                                     max=b;
+                        } 
+                        else if(c>max){
+                          
+                          max=c  ;
+      
+                        } 
+                       // return max;
+                       System.out.println("The larger number is "+max);
+    
+                      
+                }}
+              
+
